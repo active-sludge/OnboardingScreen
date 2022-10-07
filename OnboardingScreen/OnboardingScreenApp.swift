fa3f07a39273f53a0320f7e7985aad67f9b0c291
Can <hcdayan@hotmail.com>
2022-10-08 01:14:48 +0300

    Initial Commit

---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Can on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
